a414b7e41abbcb03fb5944756ad901b2c97d8fe5
AlexASC21 <87743123+Alex81405@users.noreply.github.com>
2023-06-01 18:39:21 -0400

    Update TheWelcome.vue

--- vue-project/src/components/TheWelcome.vue
+++ vue-project/src/components/TheWelcome.vue
@@ -71,4 +71,4 @@ import SupportIcon from './icons/IconSupport.vue'
     us by
     <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
   </WelcomeItem>
-</template> -->
+</template> -->.
